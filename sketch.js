f0793ce7886f87e8576d9d4c33fa53271f8a1ef9
NikitaBhatia08 <76220178+NikitaBhatia08@users.noreply.github.com>
2020-12-28 10:56:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var a ,b;
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(400, 200, 50, 50);
+  a.shapeColor = "green";
+  b= createSprite(100,200,50,40);
+  b.shapeColor = "green"
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  a.y = World.mouseY;
+  a.x= World.mouseX;
+  
+  console.log(a.x-b.x)
+  drawSprites();
+  if(a.x-b.x<a.width/2+b.width/2 && b.x-a.x<a.width/2+b.width/2 
+    && a.y-b.y<a.height/2+b.height/2  && b.y-a.y<a.height/2+b.height/2){
+    a.shapeColor="red";
+    b.shapeColor="red";
+  }
+  else{
+    a.shapeColor="green";
+    b.shapeColor="green";
+  }
+}
